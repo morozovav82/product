63d0c867b5c95eeb2b426a252df5a24fc961b554
morozov <morozov82@mail.ru>
2021-02-20 15:21:28 +0300

    включен мониторинг; actuator и swagger выставлены без аутентификации

--- src/main/java/ru/morozov/product/config/SecurityConfig.java
+++ src/main/java/ru/morozov/product/config/SecurityConfig.java
@@ -37,6 +37,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/public/**").permitAll()
                 .antMatchers("/tests/**").permitAll()
+                .antMatchers("/actuator/**").permitAll()
+                .antMatchers("/swagger*/**").permitAll()
+                .antMatchers("/v2/api-docs").permitAll()
+                .antMatchers("/webjars/**").permitAll()
                 .antMatchers(HttpMethod.POST, "/**").hasRole("MANAGER")
                 .antMatchers(HttpMethod.PUT, "/**").hasRole("MANAGER")
                 .antMatchers(HttpMethod.PATCH, "/**").hasRole("MANAGER")
@@ -68,7 +72,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         final HeadersAuthenticationFilter filter = new HeadersAuthenticationFilter(
                 new AndRequestMatcher(
                     new NegatedRequestMatcher(new AntPathRequestMatcher("/public/**")),
-                    new NegatedRequestMatcher(new AntPathRequestMatcher("/tests/**"))
+                        new NegatedRequestMatcher(new AntPathRequestMatcher("/tests/**")),
+                        new NegatedRequestMatcher(new AntPathRequestMatcher("/actuator/**")),
+                        new NegatedRequestMatcher(new AntPathRequestMatcher("/swagger*/**")),
+                        new NegatedRequestMatcher(new AntPathRequestMatcher("/v2/api-docs")),
+                        new NegatedRequestMatcher(new AntPathRequestMatcher("/webjars/**"))
                 ));
         filter.setAuthenticationManager(authenticationManager());
         return filter;
